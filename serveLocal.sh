94b96d0afa7e0bad0f709ba3d1d563604c3a2ee1
xy124 <qcd@gmx.net>
2017-10-08 11:35:45 +0200

    Serve easily

--- serveLocal.sh
+++ serveLocal.sh
@@ -2,5 +2,4 @@
 
 wget -O javascripts/easteregg.min.js https://raw.githubusercontent.com/xy124/easteregg.js/master/dist/easteregg.min.js
 
-bundle exec jekyll serve --watch --drafts
-
+bundle exec jekyll serve # --watch --drafts
